e47fb5739291830a31dc1165fa4cf0d168ef79f8
MakletsovVE <v.makletsov@yandex.ru>
2020-09-07 10:55:06 +0700

    feat: class FileIOManager has been added.
    Реализация IOManager для чтения с диска и записи в файл на диске.
    Содержит конструктор:
    
    FileIOManager(String inputFile, String outputFile)
    - бросает IOException и FileNotFoundException, если файлы недоступны или их нельзя прочесть/создать.
    
    Методы:
    
    .getReader();
    .getWriter();

---
+++ src/main/java/ru/makletsov/aikam/FileIOManager.java
@@ -0,0 +1,65 @@
+package ru.makletsov.aikam;
+
+import java.io.*;
+
+public class FileIOManager implements IOManager {
+    private final BufferedReader reader;
+    private final PrintWriter writer;
+
+    public FileIOManager(String inputFileName, String outputFileName) throws IOException {
+        if (inputFileName == null) {
+            throw new NullPointerException("Параметр имени файла входных данных = null.");
+        }
+
+        File file = new File(inputFileName);
+
+        if (!file.exists()) {
+            throw new FileNotFoundException("Не удалось найти заданный входной файл: \"" + inputFileName + "\".");
+        }
+
+        if (!file.canRead()) {
+            throw new IOException("Заданный входной файл не доступен для записи: \"" + inputFileName + "\".");
+        }
+
+        if (file.isHidden()) {
+            throw new IOException("Заданный входной файл скрыт и поэтому не доступен: \"" + inputFileName + "\".");
+        }
+
+        if (!file.isFile()) {
+            throw new FileNotFoundException("Заданный входной файл не является нормальным файлом: \"" + inputFileName +
+                    "\". " + System.lineSeparator() +
+                    "Возможно он является директорией или не удовлетворяет другим системным требованиям.");
+        }
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("Не удается открыть файл для чтения по неизвестной причине " + inputFileName + ".");
+        }
+
+        if (outputFileName == null) {
+            throw new NullPointerException("Параметр имени файла результата = null.");
+        }
+
+        try {
+            writer = new PrintWriter(outputFileName);
+        } catch (IOException e) {
+            throw new FileNotFoundException("Заданный выходной файл существует, но является дирректорией, "
+                    + System.lineSeparator() +
+                    "не существует и не может быть создан по неизвестной причине или"
+                    + System.lineSeparator() +
+                    "существует но не может быть открыт по неизвестной причине " + outputFileName + ".");
+        }
+
+    }
+
+    @Override
+    public BufferedReader getReader() {
+        return reader;
+    }
+
+    @Override
+    public PrintWriter getWriter() {
+        return writer;
+    }
+}
