f61522562119b310cd9a67f41e78587aa0ae467d
MakletsovVE <v.makletsov@yandex.ru>
2020-09-07 14:33:08 +0700

    fix: class Error (Jackson annotations has been added)

--- src/main/java/ru/makletsov/aikam/result/error/Error.java
+++ src/main/java/ru/makletsov/aikam/result/error/Error.java
@@ -1,12 +1,15 @@
 package ru.makletsov.aikam.result.error;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import ru.makletsov.aikam.result.Result;
 
+@JsonPropertyOrder({"type", "message"})
 public class Error implements Result {
     private final String type;
     private final String message;
 
-    private final String TYPE = "error";
+    private final static String TYPE = "error";
 
     public Error(String message) {
         type = TYPE;
@@ -15,10 +18,12 @@ public class Error implements Result {
     }
 
     @Override
+    @JsonGetter
     public String getType() {
         return type;
     }
 
+    @JsonGetter
     public String getMessage() {
         return message;
     }
