7562098a5ccd98a9aeece1bbf02dc539261872a4
MakletsovVE <v.makletsov@yandex.ru>
2020-09-07 12:09:41 +0700

    feat: class DateRange has been added.
    Класс реализующий сущность интервала дат для сбора статистики.
    Имеет поля:
    
    private LocalDate startDate;
    private LocalDate endDate;
    
    соответствующие геттеры и сеттеры.

---
+++ src/main/java/ru/makletsov/aikam/result/statistic/DateRange.java
@@ -0,0 +1,24 @@
+package ru.makletsov.aikam.result.statistic;
+
+import java.time.LocalDate;
+
+public class DateRange {
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
