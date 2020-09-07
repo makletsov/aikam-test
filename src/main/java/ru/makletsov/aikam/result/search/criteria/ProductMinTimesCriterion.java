4de246191719d4d05e74d7908e83f7feb57cf051
MakletsovVE <v.makletsov@yandex.ru>
2020-09-07 11:37:08 +0700

    feat: class ProductMinTimesCriterion has been added.
    Класс реализует сущность критерия поиска покупателей по минимальному количеству покупок определенного товара.
    Содержит поля:
    
    private final String productName;
    private final Integer minTimes;
    
    методы:
    
    public List<Customer> getCustomersList(Session session);
    
    Аннотирован соответствующим образом для десериализации из json.

---
+++ src/main/java/ru/makletsov/aikam/result/search/criteria/ProductMinTimesCriterion.java
@@ -0,0 +1,34 @@
+package ru.makletsov.aikam.result.search.criteria;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.Session;
+import ru.makletsov.aikam.entities.Customer;
+
+import java.util.List;
+
+public class ProductMinTimesCriterion implements Criterion {
+    private final String productName;
+    private final Integer minTimes;
+
+    @JsonCreator
+    public ProductMinTimesCriterion(@JsonProperty("productName") String productName, @JsonProperty("minTimes") Integer minTimes) {
+        this.productName = productName;
+        this.minTimes = minTimes;
+    }
+
+    @Override
+    @JsonIgnore
+    public List<Customer> getCustomersList(Session session) {
+        String queryString = String.format("SELECT c " +
+                "FROM Purchase AS p " +
+                "INNER JOIN p.customer AS c " +
+                "INNER JOIN p.product AS pr " +
+                "WHERE pr.productName = '%s' " +
+                "GROUP BY c " +
+                "HAVING COUNT(*) > %d", productName, minTimes);
+
+        return Criterion.getQuery(session, queryString).list();
+    }
+}
