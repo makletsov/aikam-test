35bfe00bd8a097548f5c530613108213fb8b64cf
MakletsovVE <v.makletsov@yandex.ru>
2020-09-07 11:31:31 +0700

    feat: class LastNameCriterion has been added.
    Класс реализует сущность критерия поиска по фамилии.
    Содержит поля:
    
    private String lastName;
    
    методы:
    
    public List<Customer> getCustomersList(Session session);
    
    Аннотирован соответствующим образом для десериализации из json.

---
+++ src/main/java/ru/makletsov/aikam/result/search/criteria/LastNameCriterion.java
@@ -0,0 +1,26 @@
+package ru.makletsov.aikam.result.search.criteria;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.Session;
+import ru.makletsov.aikam.entities.Customer;
+
+import java.util.List;
+
+public class LastNameCriterion implements Criterion {
+    private String lastName;
+
+    @JsonCreator
+    public LastNameCriterion(@JsonProperty("lastName") String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    @JsonIgnore
+    public List<Customer> getCustomersList(Session session) {
+        String queryString = String.format("FROM Customer WHERE lastName = '%s'", lastName);
+
+        return Criterion.getQuery(session, queryString).list();
+    }
+}
