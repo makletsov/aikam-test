eaaf9a9acabba42878a2c7f6cddc154112d198a9
MakletsovVE <v.makletsov@yandex.ru>
2020-09-07 12:44:55 +0700

    feat: class Error has been added.
    Класс реализующий интерфейс Result, предоставляющий информацию об ошибке
    в процессе работы программы для печати ее в отчет.
    
    Содержит поля:
    
    private final String type;
    private final String message;
    
    константы:
    
    private static String TYPE = "error";
    
    конструктор:
    
    public Error(String message)
    
    геттеры дляя полей.

---
+++ src/main/java/ru/makletsov/aikam/result/error/Error.java
@@ -0,0 +1,25 @@
+package ru.makletsov.aikam.result.error;
+
+import ru.makletsov.aikam.result.Result;
+
+public class Error implements Result {
+    private final String type;
+    private final String message;
+
+    private static String TYPE = "error";
+
+    public Error(String message) {
+        type = TYPE;
+
+        this.message = message;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
